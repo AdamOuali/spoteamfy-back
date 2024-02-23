6c0481f57089ea1e1f19ab15832833ca3249dd75
AdamOuali <ouali.adam.sailly@gmail.com>
2024-02-23 08:12:21 +0100

    feat: added animal in cluster-graph route

--- getClusterGraph.py
+++ getClusterGraph.py
@@ -5,4 +5,11 @@ from predict import predict_cluster
 def getClusterGraph(token):
     features = getFeatures(token)
     graph_data = predict_cluster(features)
+    # graph_data["animal_cluster"] = faire la moyenne du tableau dans "user_cluster"
+    values_of_user_cluster = graph_data["user_cluster"]
+    graph_data["animal_cluster"] = sum(values_of_user_cluster) / len(
+        values_of_user_cluster
+    )
+    # arrondir en int
+    graph_data["animal_cluster"] = int(graph_data["animal_cluster"])
     return graph_data
